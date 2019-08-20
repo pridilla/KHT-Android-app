a4c7ac16d0129c3169657c93fec24bef3fd5cfea
peter <ptrridilla9@gmail.com>
2019-08-20 23:16:37 +0200

    Revert "Test"
    This reverts commit 8a133896

--- app/src/main/java/com/example/khtappka/MainActivity.kt
+++ app/src/main/java/com/example/khtappka/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 // https://github.com/jkcclemens/khttp/blob/master/LICENSE
-//test
+//testkj
 
 class MainActivity : AppCompatActivity() {
 
