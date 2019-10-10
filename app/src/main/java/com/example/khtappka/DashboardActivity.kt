903c147828273b8f4bc6513150116372bc20a1b1
Peter Ridilla <ptrridilla9@gmail.com>
2019-10-10 19:09:48 +0200

    dokonceny login

--- app/src/main/java/com/example/khtappka/DashboardActivity.kt
+++ app/src/main/java/com/example/khtappka/DashboardActivity.kt
@@ -4,6 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_dashboard.*
+import okhttp3.WebSocket
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
 
 class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +21,8 @@ class DashboardActivity : AppCompatActivity() {
 
         this.textView3.text = userLocalStore.getLoggedInUser().password
         this.textView2.text = userLocalStore.getLoggedInUser().username
+
+        val socket = WebSocket("api.kosicehacktoken.sk")
 
         this.activity_logout_button.setOnClickListener {
 
--- app/src/main/java/com/example/khtappka/User.kt
+++ app/src/main/java/com/example/khtappka/User.kt
@@ -1,55 +1,59 @@
 package com.example.khtappka
 
+import android.os.AsyncTask
+import android.os.Handler
 import android.util.Log
 import com.example.khtappka.Constants.Companion.API_URL
 import java.lang.Exception
 import java.util.Map
+import kotlin.concurrent.thread
+import android.os.StrictMode
+
+
 
 class User (iusername: String, ipassword: String){
     val username = iusername
     val password = ipassword
-    var id : String? = null
-    var message: String? = null
-    var flag: String? = null
-    var email: String? = null
-
-    private lateinit var mRunnable:Runnable
+    var id = ""
+    lateinit var message: String
+    lateinit var flag: String
+    var email = ""
 
     fun login() : Boolean {
-        Log.d("KHT", "123")
-        mRunnable = Runnable {
-            val response = khttp.post(API_URL + "/login", mapOf("username" to username, "password" to password))
-            message = response.jsonObject.get("message") as String?
-            flag = response.jsonObject.get("flag") as String?
-            id = response.jsonObject.get("id") as String?
-            Log.d("KHT", message)
-        }
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+
+        val response = khttp.post(API_URL + "/login", json = mapOf("username" to username, "password" to password))
+        message = response.jsonObject.get("message") as String
+        flag = response.jsonObject.get("flag") as String
+        id = response.jsonObject.get("id").toString()
+        Log.d("KHT", message)
         if (message == "success") return true
         return false
     }
 
     fun register() : Boolean {
-        val response = khttp.post(API_URL + "/register", mapOf("username" to username, "password" to password, "email" to email, "flag" to "1"))
-        message = response.jsonObject.get("message") as String?
-        flag = response.jsonObject.get("flag") as String?
-        id = response.jsonObject.get("id") as String?
+        val response = khttp.post(API_URL + "/register", json = mapOf("username" to username, "password" to password, "email" to email, "flag" to "1"))
+        message = response.jsonObject.get("message") as String
+        flag = response.jsonObject.get("flag") as String
+        id = response.jsonObject.get("id") as String
         if (message == "success") return true
         return false
     }
 
     fun checkId() : Boolean {
-        val response = khttp.post(API_URL + "/user", mapOf("id" to id))
-        message = response.jsonObject.get("message") as String?
-        flag = response.jsonObject.get("flag") as String?
-        id = response.jsonObject.get("id") as String?
-        email = response.jsonObject.get("email") as String?
+        val response = khttp.post(API_URL + "/user", json = mapOf("id" to id))
+        message = response.jsonObject.get("message") as String
+        flag = response.jsonObject.get("flag") as String
+        id = response.jsonObject.get("id") as String
+        email = response.jsonObject.get("email") as String
         if (message == "success") return true
         return false
     }
 
     fun testUrl() : Boolean {
         val response = khttp.get(API_URL + "/test")
-        message = response.jsonObject.get("message") as String?
+        message = response.jsonObject.get("message") as String
         if (message == "success") return true
         return false
     }
